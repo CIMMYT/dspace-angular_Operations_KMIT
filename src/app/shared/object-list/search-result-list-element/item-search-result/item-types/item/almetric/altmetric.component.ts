4fc7b8b148d9a54e3d7c689bc184a729ad3b0d10
Gerardo Flores <gflores@dspce.dev.com>
2023-09-29 13:59:48 -0600

    Delete almetric css

--- src/app/shared/object-list/search-result-list-element/item-search-result/item-types/item/almetric/altmetric.component.ts
+++ src/app/shared/object-list/search-result-list-element/item-search-result/item-types/item/almetric/altmetric.component.ts
@@ -3,7 +3,6 @@ import { Component, Input } from '@angular/core';
 @Component({
   selector: 'altmetric-badge',
   templateUrl: './altmetric.component.html',
-  styleUrls: ['./altmetric.component.css']
 })
 
 export class AltmetricComponent {
